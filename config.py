6e9c9cdbbc63734af2bc5a6081a31a32ce4f0090
kr4ng <steven.m.simoni@gmail.com>
2015-07-05 07:23:59 -0400

    moved config to settings

---
+++ config.py
@@ -0,0 +1,3 @@
+# set your environment
+clearbit.key = '1bd77da89e73be5b12278a80d2fc3ff2'
+slack = Slacker('xoxp-4477022291-4457488502-7201309238-0be375')
--- merge.py
+++ merge.py
@@ -5,9 +5,7 @@ from flask import Flask, request, json
 import clearbit
 from slacker import Slacker
 
-# set your environment
-clearbit.key = '1bd77da89e73be5b12278a80d2fc3ff2'
-slack = Slacker('xoxp-4477022291-4457488502-7201309238-0be375')
+from settings import *
 
 from flask.ext import restful
 from flask.ext.jsonpify import jsonify
